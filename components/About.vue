d50123d8e0219945e72d4179dd462b5cb5973617
brazillierjo <j.brazillier@gmail.com>
2024-12-31 20:11:04 +0100

    feat: add translation for about section

--- components/About.vue
+++ components/About.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import Tag from "~/components/ui/Tag.vue";
 import { links } from "~/utils/links";
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const introduceLink = links.find((link) => link.name === "About");
 
-const paragraphs = [
-  "📚 I initially stopped studying after completing a Technological Baccalaureate. From odd jobs to odd jobs, I quickly realized that this context would not be enough for me. So, at the age of 21, I decided to resume my studies and pursue a degree in e-commerce.",
-  "🏋️ These studies allowed me to work as an SEO specialist for 6 years. This experience taught me about the functioning of the professional world, teamwork, the weight of responsibilities, as well as managing a back office and search engine optimization.",
-  "🍀 As a SEO specialist, working closely with developers sparked my interest in this highly technical field. Therefore, I enrolled in a 6-month bootcamp training program to acquire the fundamentals of web programming and coding. Following that, I continued my learning journey through an apprenticeship program, pursuing a degree in Web and Mobile Applications Integration/Development with a specialization in React and Node.js.",
-  "🍾 Since then, I have worked with several companies on complete integrations, provided assistance to deliver projects on time, made modifications, and added new features...",
-  "👷 Today, drawing strength from my journey, I master the intricacies of the Agile methodology, the principles of Clean Code, and CI/CD pipelines, ensuring optimal scalability and maintainability for each web application I develop. My diverse experience, spanning from SEO to programming, enables me to bring a unique perspective to every project.",
-  "🧘‍♂ As a Web Developer since 2020, I thoroughly enjoy working with Nuxt & NextJs in an Agile environment, adhering to clean code principles, and implementing CI/CD pipelines. I take pleasure in transforming mockups of various styles into dynamic and interactive websites to enhance the user experience.",
+const paragraphKeys = [
+  'aboutMe.phrase1',
+  'aboutMe.phrase2',
+  'aboutMe.phrase3',
+  'aboutMe.phrase4',
+  'aboutMe.phrase5',
+  'aboutMe.phrase6',
 ];
 </script>
 
@@ -24,16 +27,16 @@ const paragraphs = [
       {{ introduceLink?.name }}
     </Tag>
 
-    <h1 class="text-2xl font-bold" v-motion-fade>my <span class="text-primary">path</span>.</h1>
+    <h1 class="text-2xl font-bold" v-motion-fade>{{ t('aboutMe.title1') }} <span class="text-primary">{{ t('aboutMe.title2') }}</span>.</h1>
 
     <div class="text-md flex flex-col gap-6">
       <p
-        v-for="(text, index) in paragraphs"
+        v-for="(key, index) in paragraphKeys"
         :key="index"
         class="text-justify xl:text-text"
         v-motion-slide-visible-right
       >
-        {{ text }}
+        {{ t(key) }}
       </p>
     </div>
 
